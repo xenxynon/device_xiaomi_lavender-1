b31d52455fe5496370dd0b8eb075ccc46063558b
Prashant <93838092+ImPrashantt@users.noreply.github.com>
2022-03-15 17:56:22 +0530

    slap my name as device memetainer
    and use redfin keys as well

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -13,10 +13,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-PEX_BUILD_TYPE := OFFICIAL
-PEX_MAINTAINER := Prashant
+TARGET_SUPPORTS_GOOGLE_RECORDER := false
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+PEX_MAINTAINER := Mayur ◢◤
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -32,10 +32,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="lavender" \
     PRODUCT_NAME="lavender" \
-    PRIVATE_BUILD_DESC="coral-user 11 RQ3A.211001.001 7641976 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
 
-BUILD_FINGERPRINT :="google/coral/coral:11/RQ3A.211001.001/7641976:user/release-keys"
+BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
 
 # Pixel Extended Device Maintainers
 PRODUCT_PROPERTY_OVERRIDES += \
-    org.pex.build_maintainer=Prashant
+    org.pex.build_maintainer=Mayur ◢◤
