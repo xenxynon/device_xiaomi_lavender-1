ddfb8539655ac5c5b825b9f3b9605450d18be225
xenxynon <flynryder427@gmail.com>
2022-04-29 04:08:34 +0000

    refactor to superior
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- superior_lavender.mk
+++ superior_lavender.mk
@@ -9,20 +9,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common PixelExtended stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk) 
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
-TARGET_INCLUDE_STOCK_ACORE := false
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-PEX_MAINTAINER := Mayur ◢◤
+# Inherit some common stuff
+$(call inherit-product, vendor/superior/config/common.mk)
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Inherit from lavender device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_lavender
+PRODUCT_NAME := superior_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
@@ -35,7 +31,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001 7641976 release-keys"
 
 BUILD_FINGERPRINT :="google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
-
-# Pixel Extended Device Maintainers
-PRODUCT_PROPERTY_OVERRIDES += \
-    org.pex.build_maintainer=Mayur ◢◤
