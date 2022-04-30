3a8cd54bf412cf271f6b2cb1998909f9244cd098
xenxynon <flynryder427@gmail.com>
2022-04-30 06:19:30 +0000

    lavender: cleanup * drop additional kernel flags * correct cpu variant * drop unnecessary sepolicy
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@ TARGET_CPU_VARIANT := cortex-a73
 
 ifeq (,$(filter %_64,$(TARGET_PRODUCT)))
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
@@ -48,8 +48,6 @@ TARGET_KERNEL_CLANG_COMPILE := true
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm660
-TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
